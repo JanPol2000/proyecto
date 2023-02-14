80c21f6f2e0f64173e7770bfbc61a38bb363e631
JanPol <dark2000jp@gmail.com>
2023-02-14 13:54:27 -0600

    Puntos de reunion Problema inesperado con CORS v2

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -139,7 +139,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
